64d511f0c0d1deaa641af801c58ae5c33ec74dc3
Duncan McGreggor <oubiwann@gmail.com>
2022-11-12 01:53:10 -0600

    Added parsers, refactored.

--- src/json.rs
+++ src/json.rs
@@ -1,7 +1,21 @@
-pub fn parse_file() {
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+use serde_json;
+use crate::schema;
 
+pub fn parse_file<P: AsRef<Path>>(path: P) -> Result<schema::Game, Box<dyn Error>> {
+    let file =  File::open(path)?;
+    let reader = BufReader::new(file);
+    let g = serde_json::from_reader(reader)?;
+    Ok(g)
 }
 
-pub fn parse_str() {
+pub fn parse_bytes<'a>(data: &'a [u8]) -> serde_json::Result<schema::Game> {
+    serde_json::from_slice(data)
+}
 
+pub fn parse_str<'a>(data: &'a str) -> serde_json::Result<schema::Game> {
+    serde_json::from_str(data)
 }
--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -1,10 +1,53 @@
-use twyne::twine::{Game};
+use std::fs;
+use std::path::Path;
+use twyne::schema;
 
-pub fn lobby_game() -> Game {
-    let data = include_str!("../../resources/samples/lobby-game.json");
-    let g: Game = match serde_json::from_str(data) {
+pub const LOBBY_GAME: &str = "resources/samples/lobby-game.json";
+
+#[allow(dead_code)]
+pub fn lobby_game_data_str() -> String {
+    fs::read_to_string(Path::new(LOBBY_GAME)).unwrap()
+}
+
+#[allow(dead_code)]
+pub fn lobby_game_data_bytes() -> Vec<u8> {
+    fs::read(Path::new(LOBBY_GAME)).unwrap()
+}
+
+#[allow(dead_code)]
+pub fn lobby_game() -> schema::Game {
+    let data = lobby_game_data_str();
+    let g: schema::Game = match serde_json::from_str(&data) {
         Ok(parsed) => parsed,
         Err(error) => panic!("Tests can't run without the sample file: {:?}", error),
     };
     g
+}
+
+#[allow(dead_code)]
+pub fn check_all_data(game: &schema::Game) {
+    check_game_data(game);
+    check_passages_data(game);
+    check_links_data(game);
+}
+
+#[allow(dead_code)]
+pub fn check_game_data(game: &schema::Game) {
+    assert_eq!(game.name, "Lobby");
+}
+
+#[allow(dead_code)]
+pub fn check_passages_data(game: &schema::Game) {
+    assert_eq!(game.passages.len(), 6);
+    let passage: schema::Passage = game.passages[0].clone();
+    let text: String = passage.text.chars().take(31).collect();
+    assert_eq!(text, "You are in a lobby -- THE lobby");
+}
+
+#[allow(dead_code)]
+pub fn check_links_data(game: &schema::Game) {
+    let passage: schema::Passage = game.passages[0].clone();
+    let link: schema::Link = passage.links[0].clone();
+    assert_eq!(link.link_text, "Create Character");
+    assert_eq!(link.passage_name, "Create Character");
 }
